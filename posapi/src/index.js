405af0cacbe185391dd9c3d92c8ce80f8d762a83
Hugh Ferguson <hugh@hferguson.ca>
2023-08-04 17:12:13 -0400

    Added comments to trigger a pull request

--- posapi/src/index.js
+++ posapi/src/index.js
@@ -1,4 +1,7 @@
-
+/**
+ * This is the Position API service.  Service is launched from here
+ * but the coding is inside app.js. That separation was done to facilitate testing
+ */
 const app = require('./app');
 
 let port = 3000 || process.env.PORT;
