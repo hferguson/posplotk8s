1dd99ae3dbeb96e385ff7979d173947af13b8464
Hugh Ferguson <hugh@hferguson.ca>
2023-08-06 09:54:42 -0400

    Added extra verbiage, new pages, and some CSS to support dark mode

---
+++ client/components/Footer.js
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <hr/>
+            <a href="/" className="link-info">Home</a><br/>
+            &copy; 2023 Hugh Ferguson Consulting Ltd.<br/>
+            for more information, email me at <a href="mailto:info@hferguson.ca">info@hferguson.ca</a>
+        </div>
+    )
+};
+export default Footer;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../components/MapPanel.css';
 import '../components/PlottingMap.css';
+import './page.css';
 /**
  * The purpose of this file is to apply a bootstrap "wrapper" around each page in this
  * pages directory.  Effectively the value of Component is our page (i.e. index.js). NextJS
